354101c880b5756f63091540efd727306da6eed4
Tim Chen Allen <timothychenallen@gmail.com>
2025-10-02 14:40:49 -0400

    Tim/feat/02-reusable-listitem-component init

--- tim/app/page.tsx
+++ tim/app/page.tsx
@@ -1,24 +1,38 @@
+// Challenge 02 - timothy.allen@iem.com
+"use client";
 import React from 'react';
 
+// A presenter component
+function ListItem({ title, desc , onSelect } : { title: string; desc: string; onSelect?: () => void }) {
+	return (
+		<button onClick={onSelect}>
+			{title}
+		</button>
+	);
+}
+
 export default function Home() {
-	const items = [
+	// Keep data separate from presentation
+	const heroes = [
 		{ id: 1, title: 'Legolas', desc: 'A Wood Elf', strength: 'Bow' },
 		{ id: 2, title: 'Gimli', desc: 'A Dward', strength: 'Axe' },
 		{ id: 3, title: 'Merry', desc: 'A Hobbit', strength: 'Courage' },
 		{ id: 4, title: 'Pippin', desc: 'A SecondHobbit', strength: 'Stomach' }
 	];
-	const active = items[0];
+	const active = heroes[0];
 
+	// Call the presenter component from here
+	// this interactivity is temporary, just trying out passing an event handler
 	return (
 		<div>
-			<h1>Challenge 01 [Tim]</h1>
 			<ul>
-				{items.map(item => (
-					<li key={item.id}>
-					{item.title}
+				{heroes.map(hero => (
+					<li key={hero.id}>
+					<ListItem title={hero.title} desc={hero.desc} onSelect={ ()=>{window.alert("You clicked " + hero.title )} } />
 					</li>
 				))}
 			</ul>
+			<hr/>
 			<div>
 				<h2>{active.title}</h2>
 				<p>{active.desc}</p>
